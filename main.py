9875626556273b4ebe15c8619fdd23936058a2ed
Moulishwaran Balaji <moulish.developer@gmail.com>
2024-12-21 09:15:41 +0000

    edited the indent bug and added opencv

--- main.py
+++ main.py
@@ -24,37 +24,37 @@ class SecurityCamera:
 
         self.update_video_feed()
 
-        def update_video_feed():
-            
-            #reading frames from the camera
-            # capture_Boolean gives boolean value if the video is being captured or not
-            # frame is the numpy array of the video
-            capture_Boolean, frame = self.cap.read()
-            if capture_Boolean:
-                # converting frame to RGB
-                frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-                img = Image.fromarray(frame)
-                imgtk = ImageTk.PhotoImage(image=img)
-
-                #upddate the video label which is initaited before 
-                self.video_label.imgtk = imgtk
-                self.video_label.configure(image=imgtk)
-
-            # refresh and repeating - 100ms
-            self.root.after(100, self.update_video_feed)
-
-        def take_snapshot(self):
-            img_Boolean, frame = self.cap.read()
-            if img_Boolean:
-                cv2.imwrite("snapshot.jpg",frame)
-                print("Snapshot saved as snapshot.jpg")
+    def update_video_feed(self):
         
-        def exit_app(self):
-            #release the camera and closing all
-            self.cap.release()
-            self.root.destroy()
+        #reading frames from the camera
+        # capture_Boolean gives boolean value if the video is being captured or not
+        # frame is the numpy array of the video
+        capture_Boolean, frame = self.cap.read()
+        if capture_Boolean:
+            # converting frame to RGB
+            frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(frame)
+            imgtk = ImageTk.PhotoImage(image=img)
 
+            #upddate the video label which is initaited before 
+            self.video_label.imgtk = imgtk
+            self.video_label.configure(image=imgtk)
+
+        # refresh and repeating - 100ms
+        self.root.after(100, self.update_video_feed)
+
+    def take_snapshot(self):
+        img_Boolean, frame = self.cap.read()
+        if img_Boolean:
+            cv2.imwrite("snapshot.jpg",frame)
+            print("Snapshot saved as snapshot.jpg")
     
+    def exit_app(self):
+        #release the camera and closing all
+        self.cap.release()
+        self.root.destroy()
+
+
 if __name__== "__main__":
     root=tk.Tk()
     app = SecurityCamera(root)
