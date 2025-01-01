7a37fd6b73835934f6ffc0b77b23178a7e880968
Moulishwaran Balaji <moulish.developer@gmail.com>
2025-01-01 08:58:17 +0000

    added video recording feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-import threading
+import datetime
 import tkinter as tk
 from tkinter import Button, Label
 from PIL import Image, ImageTk
@@ -11,10 +11,15 @@ class SecurityCamera:
 
         #Video Capturing
         self.cap = cv2.VideoCapture(0)
+        self.is_recording = False
+        self.video_writer = None
 
         #label to display the video feedpip
         self.video_label = tk.Label(root)
         self.video_label.pack()
+
+        self.btn_recording = tk.Button(root, text="Start Recording", command=self.start_recording)
+        self.btn_recording.pack(pady=10)
 
         self.btn_snapshot = tk.Button(root, text="Take Snapshot", command=self.take_snapshot)
         self.btn_snapshot.pack(pady=10)
@@ -25,7 +30,6 @@ class SecurityCamera:
         self.update_video_feed()
 
     def update_video_feed(self):
-        
         #reading frames from the camera
         # capture_Boolean gives boolean value if the video is being captured or not
         # frame is the numpy array of the video
@@ -40,19 +44,42 @@ class SecurityCamera:
             self.video_label.imgtk = imgtk
             self.video_label.configure(image=imgtk)
 
+        #if video recording is on then frames is passed on
+        if self.is_recording and self.video_writer is not None:
+            self.video_writer.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+
+
         # refresh and repeating - 100ms
         self.root.after(100, self.update_video_feed)
 
     def take_snapshot(self):
         img_Boolean, frame = self.cap.read()
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
         if img_Boolean:
-            cv2.imwrite("snapshot.jpg",frame)
+            cv2.imwrite(f"snapshots/snapshot_{timestamp}.jpg",frame)
             print("Snapshot saved as snapshot.jpg")
     
     def exit_app(self):
         #release the camera and closing all
         self.cap.release()
         self.root.destroy()
+
+    def start_recording(self):
+        
+        if not self.is_recording:
+            self.is_recording = True
+            self.btn_recording.config(text="Stop Recording")
+            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+            timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            self.video_writer = cv2.VideoWriter(f'recordings/recording_{timestamp}.mp4',fourcc,20.0,(640,480))
+        else:
+            self.is_recording = False
+            self.btn_recording.config(text="Start Recording")
+            if self.video_writer is not None:
+                self.video_writer.release()
+                self.video_writer = None
+
+
 
 
 if __name__== "__main__":
